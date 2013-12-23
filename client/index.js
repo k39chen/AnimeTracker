94a01fdb57922a2a44bb848d1ae66821ecbe1031
Kevin Chen <Kevin@KevinChen.(none)>
2013-12-22 21:42:18 -0500

    Added xml2js

--- client/index.js
+++ client/index.js
@@ -1,3 +1,14 @@
 Template.animelist.list = function() {
-	return Animes.find();
+    return Animes.find();
 }
+
+$(document).ready(function(){
+
+
+    Meteor.call('getSampleXMLResponse', function(err,result){
+        console.log(result);
+        $('body').append(JSON.stringify(result));
+    });
+
+
+});
--- server/environment.js
+++ server/environment.js
@@ -1,27 +1,16 @@
-Meteor.startup(function(){
-	console.log("Hello World!");
+var require = Npm.require;
 
-	parseXml();
-	startCronJob();
+Meteor.startup(function(){
+    console.log("Hello World!");
+
+    //startCronJob();
 });
 
-function parseXml() {
-
-	var strXML = "<root>" +
-        "<group>" +
-            "<item name=\"test1\">test data 1</item>"+
-            "<item name=\"test2\">test data 2</item>"+
-            "<item name=\"test3\">test data 3</item>"+
-        "</group>"+
-    "</root>";
-
-}
-
 function startCronJob() {
-	var job = new Cron.Job(new Cron.Spec('*/2 * * * * *'), function(){
-		console.log(new Date());
-	});
-	var cron = new Cron();
-	cron.start();
-	cron.add(job);
+    var job = new Cron.Job(new Cron.Spec('*/2 * * * * *'), function(){
+        console.log(new Date());
+    });
+    var cron = new Cron();
+    cron.start();
+    cron.add(job);
 }
