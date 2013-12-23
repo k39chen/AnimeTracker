d3109f24d29acced3135651c95b5354765a61e88
Kevin Chen <Kevin@KevinChen.(none)>
2013-12-22 22:48:24 -0500

    Initial API work

---
+++ server/lib/ann_api.js
@@ -0,0 +1,32 @@
+
+var require = Npm.require,
+    xml2js = require('xml2js'),
+    Future = require('fibers/future');
+
+Meteor.methods({
+
+    getSampleXMLResponse: function(){
+
+        var future = new Future();
+
+        var data = "<root>" +
+            "<group>" +
+                "<item name=\"test1\">test data 1</item>"+
+                "<item name=\"test2\">test data 2</item>"+
+                "<item name=\"test3\">test data 3</item>"+
+            "</group>"+
+        "</root>";
+
+        var parser = new xml2js.Parser({trim:true});
+
+        parser.parseString(data, function(err,result){
+            console.log(result);
+            // return the parsed response
+            future['return'](result);
+        });
+
+        // wait for async to finish before returning the result
+        return future.wait();
+    }
+
+});
